905778dd961e81fef128e1a4d183aed9389b1d10
keyian <keyian@nyu.edu>
2020-05-14 17:13:14 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Library Angel`,
+    author: `Keyian Vafai`,
+    description: `Repository for dilettante history.`,
+    siteUrl: `https://libraryangel.club/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
+      facebook: ``,
+      instagram: ``,
     },
   },
   plugins: [
